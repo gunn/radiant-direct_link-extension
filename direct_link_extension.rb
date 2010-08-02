a56e53d94a4106df703687740719f98dc0cf6c1b
Arthur Gunn <arthur@gunn.co.nz>
2010-08-02 13:22:40 +1200

    First commit! Added barebones extension stuff.

---
+++ direct_link_extension.rb
@@ -0,0 +1,10 @@
+class DirectLinkExtension < Radiant::Extension
+  version "0.1"
+  description "Adds direct links to your pages from the tree view."
+  url "http://gunn.co.nz/"
+  
+  def activate
+    # 
+  end
+  
+end
